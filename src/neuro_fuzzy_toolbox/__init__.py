4e3c8ce330fb5ef0b35ae46f801c6d7239403787
edge-mirage <juan.suareza@usm.cl>
2026-05-30 14:19:57 -0400

    some fixes for PYPI

--- src/neuro_fuzzy_toolbox/__init__.py
+++ src/neuro_fuzzy_toolbox/__init__.py
@@ -3,3 +3,10 @@ from .layers import FuzzificationLayer, h_FuzzificationLayer, rule_reduced_Fuzzi
 from .models import ANFIS, h_ANFIS, rule_reduced_ANFIS
 from .training import SONFIS, Hybrid_learning_algorithm, Basic_optimizer_training_algorithm, Double_optimizer_training_algorithm, EarlyStopping
 from .rule_analyzer import RulesAnalyzer
+
+from importlib.metadata import version as _version, PackageNotFoundError as _PackageNotFoundError
+
+try:
+    __version__ = _version("neuro-fuzzy-toolbox")
+except _PackageNotFoundError:
+    __version__ = "unknown"
